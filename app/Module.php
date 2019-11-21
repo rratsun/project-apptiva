0db484084a7929c1a296ce76569f6c1aa260c26c
r.ratsun <r.ratsun@gmail.com>
2019-11-21 09:04:27 +0200

    init

---
+++ app/Module.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ProjectApptiva
+ * Premium Plugin
+ * Copyright (c) TreoLabs GmbH
+ *
+ * This Software is the property of TreoLabs GmbH and is protected
+ * by copyright law - it is NOT Freeware and can be used only in one project
+ * under a proprietary license, which is delivered along with this program.
+ * If not, see <http://treopim.com/eula>.
+ *
+ * This Software is distributed as is, with LIMITED WARRANTY AND LIABILITY.
+ * Any unauthorised use of this Software without a valid license is
+ * a violation of the License Agreement.
+ *
+ * According to the terms of the license you shall not resell, sublicense,
+ * rent, lease, distribute or otherwise transfer rights or usage of this
+ * Software or its derivatives. You may modify the code of this Software
+ * for your own needs, if source code is provided.
+ */
+
+declare(strict_types=1);
+
+namespace ProjectApptiva;
+
+use Treo\Core\ModuleManager\AbstractModule;
+
+/**
+ * Class Module
+ *
+ * @author r.ratsun@treolabs.com
+ */
+class Module extends AbstractModule
+{
+    /**
+     * @inheritdoc
+     */
+    public static function getLoadOrder(): int
+    {
+        return 9999;
+    }
+}
