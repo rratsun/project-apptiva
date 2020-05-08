1cd2bd293d1bd8a390897d9d43bc1973dfeac1ea
r.ratsun <r.ratsun@gmail.com>
2020-05-08 08:58:08 +0300

    Add attribute before updating attribute value

--- app/Services/MassActions.php
+++ app/Services/MassActions.php
@@ -15,6 +15,18 @@ class MassActions extends Base
     /**
      * @inheritDoc
      */
+    protected function getMassActionIds(string $entityType, \stdClass $data): array
+    {
+        if ($entityType == 'ProductAttributeValue') {
+            $this->attachAttribute($data);
+        }
+
+        return parent::getMassActionIds($entityType, $data);
+    }
+
+    /**
+     * @inheritDoc
+     */
     protected function getSelectParams(string $entityType, array $where): array
     {
         $selectParams = parent::getSelectParams($entityType, $where);
@@ -26,5 +38,67 @@ class MassActions extends Base
         }
 
         return $selectParams;
+    }
+
+    /**
+     * @param \stdClass $data
+     *
+     * @return bool
+     */
+    protected function attachAttribute(\stdClass $data): bool
+    {
+        // prepare params
+        $params = json_decode(json_encode($data), true);
+
+        if (empty($params['where'])) {
+            return false;
+        }
+
+        foreach ($params['where'] as $row) {
+            if ($row['attribute'] == 'attributeId' && $row['type'] == 'equals') {
+                $attributeId = $row['value'];
+            }
+
+            if ($row['attribute'] == 'productId' && $row['type'] == 'in') {
+                $productsIds = $row['value'];
+            }
+        }
+
+        if (empty($attributeId) || empty($productsIds)) {
+            return false;
+        }
+
+        $repository = $this->getRepository('ProductAttributeValue');
+
+        $pavs = $repository
+            ->select(['productId'])
+            ->where(
+                [
+                    'attributeId' => $attributeId,
+                    'productId'   => $productsIds,
+                    'scope'       => 'Global'
+                ]
+            )
+            ->find()
+            ->toArray();
+
+        $exists = array_column($pavs, 'productId');
+
+        foreach ($productsIds as $productId) {
+            if (!in_array($productId, $exists)) {
+                $pav = $repository->get();
+                $pav->set(
+                    [
+                        'attributeId' => $attributeId,
+                        'productId'   => $productId,
+                        'scope'       => 'Global'
+                    ]
+                );
+
+                $this->getEntityManager()->saveEntity($pav);
+            }
+        }
+
+        return true;
     }
 }
